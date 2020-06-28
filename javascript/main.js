4eea2384353ed28db5a366c0595062bc2da1d514
Steven Samuel Marroquín González <samy23033@gmail.com>
2020-06-28 11:32:36 -0600

    se agregó un botón de 'ir hacia arriba', funcional, con animaciones, el color está en decisión aún

--- javascript/main.js
+++ javascript/main.js
@@ -14,3 +14,25 @@ document.getElementById("nav-button").addEventListener("click", function() {
         aux = 0;
     }
 });
+
+const scrollToTop = document.getElementById("scroll-to-top");
+let dataShow = false;
+
+window.addEventListener("scroll", ()=>{
+    if (window.scrollY > 50 && !dataShow) {
+        scrollToTop.setAttribute("data-show", "true");
+        dataShow = true;
+    }
+
+    if (window.scrollY <= 50 && dataShow) {
+        scrollToTop.setAttribute("data-show", "false");
+        dataShow = false;
+    }
+});
+
+scrollToTop.addEventListener("click", ()=>{
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
