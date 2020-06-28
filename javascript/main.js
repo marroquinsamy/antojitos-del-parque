7b400de7e1d1f486913ba6221ffde526c1080e8c
Steven Samuel Marroquín González <samy23033@gmail.com>
2020-06-28 11:01:37 -0600

    mejoras en la barra de navegación, se añadieron sombras, color rojo al botón de cierre

--- javascript/main.js
+++ javascript/main.js
@@ -3,11 +3,13 @@ document.getElementById("nav-button").addEventListener("click", function() {
     if (aux == 0) {
         document.getElementById("hidden-nav").style.display = "flex";
         document.getElementById("nav-button").classList.remove("fa-ellipsis-h");
+        document.getElementById("nav-button").style.color = "#f28b82";
         document.getElementById("nav-button").classList.add("fa-window-close");
         aux++;
     } else {
         document.getElementById("hidden-nav").style.display = "none";
         document.getElementById("nav-button").classList.remove("fa-window-close");
+        document.getElementById("nav-button").style.color = "inherit";
         document.getElementById("nav-button").classList.add("fa-ellipsis-h");
         aux = 0;
     }
