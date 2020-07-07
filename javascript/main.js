9c18479c226db435b826c2be91f21087e2ee46c2
Steven Samuel Marroquín González <samy23033@gmail.com>
2020-07-06 18:18:36 -0600

    Se reescribió el código con fin de cambiar el div que contenía los links de navegación por un nav. Se añadieron meta tags como autor, descripción, keywords.

--- javascript/main.js
+++ javascript/main.js
@@ -1,47 +1,51 @@
-let aux = 0;
-let navButton = document.getElementById("nav-button");
-let hiddenNav = document.getElementById("hidden-nav");
-let content = document.getElementsByClassName("content");
-console.log(content);
+let aux = 0
+let navButton = document.getElementById("nav-button")
+let nav = document.getElementsByTagName("nav")
+let content = document.getElementsByClassName("content")
+console.log(nav)
 navButton.addEventListener("click", function() {
     if (aux == 0) {
-        hiddenNav.style.top = "0";
-        navButton.classList.remove("fa-ellipsis-h");
-        navButton.style.color = "#f28b82";
-        navButton.classList.add("fa-window-close");
         for (let i = 0; i < content.length; i++) {
-            content[i].style.opacity = ".3";
+            nav[i].style.top = "0"
         }
-        aux++;
+        navButton.classList.remove("fa-ellipsis-h")
+        navButton.style.color = "#f28b82"
+        navButton.classList.add("fa-window-close")
+        for (let i = 0; i < content.length; i++) {
+            content[i].style.opacity = ".3"
+        }
+        aux++
     } else {
-        hiddenNav.style.top = "-230px";
-        navButton.classList.remove("fa-window-close");
-        navButton.style.color = "inherit";
-        navButton.classList.add("fa-ellipsis-h");
-        for (let i = 0; i < content.length; i++) {
-            content[i].style.opacity = "1";
+        for (let i = 0; i < nav.length; i++) {
+            nav[i].style.top = "-230px"
         }
-        aux = 0;
+        navButton.classList.remove("fa-window-close")
+        navButton.style.color = "inherit"
+        navButton.classList.add("fa-ellipsis-h")
+        for (let i = 0; i < content.length; i++) {
+            content[i].style.opacity = "1"
+        }
+        aux = 0
     }
-});
+})
 
-const scrollToTop = document.getElementById("scroll-to-top");
-let dataShow = false;
+const scrollToTop = document.getElementById("scroll-to-top")
+let dataShow = false
 window.addEventListener("scroll", ()=>{
     if (window.scrollY > 50 && !dataShow) {
-        scrollToTop.setAttribute("data-show", "true");
-        dataShow = true;
+        scrollToTop.setAttribute("data-show", "true")
+        dataShow = true
     }
 
     if (window.scrollY <= 50 && dataShow) {
-        scrollToTop.setAttribute("data-show", "false");
-        dataShow = false;
+        scrollToTop.setAttribute("data-show", "false")
+        dataShow = false
     }
-});
+})
 
 scrollToTop.addEventListener("click", ()=>{
     window.scrollTo({
         top: 0,
         behavior: "smooth"
-    });
-});
+    })
+})
